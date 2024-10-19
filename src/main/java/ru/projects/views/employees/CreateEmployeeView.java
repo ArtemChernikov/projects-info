677112ddf5ff362dbf705bdc60607ea8b3f63dc2
Artem Chernikov <overcomingjunk@yandex.ru>
2024-10-19 23:51:42 +0300

    fix: рефакторинг CreateEmployeeView

--- src/main/java/ru/projects/views/employees/CreateEmployeeView.java
+++ src/main/java/ru/projects/views/employees/CreateEmployeeView.java
@@ -1,6 +1,5 @@
 package ru.projects.views.employees;
 
-
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
