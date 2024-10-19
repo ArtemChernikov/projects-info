1ba2586211fd81ff6644f1da7ddf55dd3a638c59
Artem Chernikov <overcomingjunk@yandex.ru>
2024-10-19 11:54:35 +0300

    init commit

---
+++ src/main/java/ru/projects/ProjectsInfoApplication.java
@@ -0,0 +1,13 @@
+package ru.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/projects/ProjectsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
