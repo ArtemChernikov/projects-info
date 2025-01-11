be7cf4b3f183ebe47b0d09ab9ec9f816219f2f25
Artem Chernikov <overcomingjunk@yandex.ru>
2025-01-11 18:38:40 +0300

    fix: доработка схемы БД

--- src/main/resources/db/changelog/scripts/010_ddl_create_table_projects_employees.sql
+++ src/main/resources/db/changelog/scripts/010_ddl_create_table_projects_employees.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS projects_employees
     id          BIGSERIAL PRIMARY KEY,
     project_id  BIGINT NOT NULL,
     employee_id BIGINT NOT NULL,
+    UNIQUE (project_id, employee_id),
     CONSTRAINT fk_project_id FOREIGN KEY (project_id) REFERENCES projects (project_id),
     CONSTRAINT fk_employee_id FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
 );
